cf209b154289ca0337d797d13877e03b4072ee2c
magnito2 <magnusotwani@gmail.com>
2020-02-22 20:51:31 +0300

    eslinting and fixes

--- app/lib/components/App.js
+++ app/lib/components/App.js
@@ -6,16 +6,19 @@ import Register from './auth/Register';
 import Room from './Room';
 import Landing from './Landing';
 
-const App = () => {
-  return (
-    <Router>
-      <div className="app">
-        <Route exact path="/" component={Room} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-      </div>
-    </Router>
-  );
-}
+const App = () =>
+{
+
+	return (
+		<Router>
+			<div className='app'>
+				<Route exact path='/' component={Landing} />
+				<Route exact path='/register' component={Register} />
+				<Route exact path='/login' component={Login} />
+				<Route exact path='/room' component={Room}/>
+			</div>
+		</Router>
+	);
+};
 
 export default App;
