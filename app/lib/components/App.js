3729402eff097a6c4cf60937fb54fa3c24460ace
magnito2 <magnusotwani@gmail.com>
2020-02-29 14:00:58 +0300

    adding authentication

--- app/lib/components/App.js
+++ app/lib/components/App.js
@@ -1,21 +1,55 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
+import { setAuthToken } from '../utils';
+import { setCurrentUser, logoutUser } from '../redux/authActions';
+import store from '../redux/store';
 
 import Login from './auth/Login';
 import Register from './auth/Register';
 import Room from './Room';
 import Landing from './Landing';
+import PrivateRoute from './auth/PrivateRoute';
+
+// Check for token to keep user logged in
+if (localStorage.jwtToken)
+{
+	// Set auth token header auth
+	const token = localStorage.jwtToken;
+
+	setAuthToken(token);
+
+	// Decode token and get user info and exp
+	const decoded = jwtDecode(token);
+
+	// Set user and isAuthenticated
+	store.dispatch(setCurrentUser(decoded));
+
+	// Check for expired token
+	const currentTime = Date.now() / 1000; // to get in milliseconds
+
+	if (decoded.exp < currentTime)
+	{
+		// Logout user
+		store.dispatch(logoutUser());
+
+		// Redirect to login
+		window.location.href = './login';
+	}
+}
 
 const App = () =>
 {
 
 	return (
 		<Router>
-			<div className='app'>
+			<div data-component='App'>
 				<Route exact path='/' component={Landing} />
 				<Route exact path='/register' component={Register} />
 				<Route exact path='/login' component={Login} />
-				<Route exact path='/room' component={Room}/>
+				<Switch>
+					<PrivateRoute exact path='/room' component={Room}/>
+				</Switch>
 			</div>
 		</Router>
 	);
--- app/lib/components/Landing.js
+++ app/lib/components/Landing.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const Landing = () => (
-	<div className='container'>
-		<h1>Hello world </h1>
-	</div>
-);
+import { Container, Jumbotron, Row, Col, Card, ButtonToolbar, Button } from 'react-bootstrap';
 
-export default Landing;
+class Landing extends Component
+{
+	constructor()
+	{
+		super();
+		this.state = {};
+	}
+
+	render()
+	{
+		return (
+			<div data-component='Landing'>
+				<Container>
+					<Jumbotron>
+						<h1>Be Awesome, create something nice </h1>
+					</Jumbotron>
+
+					<Row className='justify-content-md-center'>
+						<Col xs={6}>
+							<Card>
+								<Card.Body>
+									<Card.Title>Card title</Card.Title>
+									<h6 className='card-subtitle mb-2 text-muted'>Card subtitle</h6>
+									<p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card&apos;s content.</p>
+									{
+										this.props.auth.isAuthenticated ?
+											<Button variant='success'>Join Room</Button> :
+											<ButtonToolbar>
+												<Button variant='primary'>Login</Button>
+												<Button variant='secondary'>Sign Up</Button>
+											</ButtonToolbar>
+									}
+								</Card.Body>
+							</Card>
+						</Col>
+					</Row>
+				</Container>
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = (state) => ({
+	auth : state.auth
+});
+
+export default connect(
+	mapStateToProps,
+	{}
+)(Landing);
