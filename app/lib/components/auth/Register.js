5c6bebed5347a59ffdd04b2c1ab4f6fcf75e89a1
magnito2 <magnusotwani@gmail.com>
2020-03-09 15:22:06 +0300

    styling and refactoring

--- app/lib/components/auth/Register.js
+++ app/lib/components/auth/Register.js
@@ -58,91 +58,59 @@ class Register extends Component
 		const { errors } = this.state;
 
 		return (
-			<div className='container'>
-				<div className='row'>
-					<div className='col s8 offset-s2'>
-						<Link to='/' className='btn-flat waves-effect'>
-							<i className='material-icons left'>keyboard_backspace</i> Back to
-							home
-						</Link>
-						<div className='col s12' style={{ paddingLeft: '11.250px' }}>
-							<h4>
-								<b>Register</b> below
-							</h4>
-							<p className='grey-text text-darken-1'>
-								Already have an account? <Link to='/login'>Log in</Link>
-							</p>
+			<div data-component='Login'>
+				<div className='login-page'>
+					<div className='form'>
+						<div className='home-link'>
+							<p className='message'><Link to='/' className='icon-left'>Go Home</Link></p>
 						</div>
-						<form noValidate onSubmit={this.handleOnSubmit}>
-							<div className='input-field col s12'>
-								<input
-									onChange={this.handleOnChange}
-									value={this.state.name}
-									error={errors.name}
-									id='name'
-									type='text'
-									className={classnames('', {
-										invalid : errors.name
-									})}
-								/>
-								<label htmlFor='name'>Name</label>
-								<span className='red-text'>{errors.name}</span>
-							</div>
-							<div className='input-field col s12'>
-								<input
-									onChange={this.handleOnChange}
-									value={this.state.email}
-									error={errors.email}
-									id='email'
-									type='email'
-									className={classnames('', {
-										invalid : errors.email
-									})}
-								/>
-								<label htmlFor='email'>Email</label>
-								<span className='red-text'>{errors.email}</span>
-							</div>
-							<div className='input-field col s12'>
-								<input
-									onChange={this.handleOnChange}
-									value={this.state.password}
-									error={errors.password}
-									id='password'
-									type='password'
-									className={classnames('', {
-										invalid : errors.password
-									})}
-								/>
-								<label htmlFor='password'>Password</label>
-							</div>
-							<div className='input-field col s12'>
-								<input
-									onChange={this.handleOnChange}
-									value={this.state.password2}
-									error={errors.password2}
-									id='password2'
-									type='password'
-									className={classnames('', {
-										invalid : errors.password2
-									})}
-								/>
-								<label htmlFor='password2'>Confirm Password</label>
-								<span className='red-text'>{errors.password2}</span>
-							</div>
-							<div className='col s12' style={{ paddingLeft: '11.250px' }}>
-								<button
-									style={{
-										width         : '150px',
-										borderRadius  : '3px',
-										letterSpacing : '1.5px',
-										marginTop     : '1rem'
-									}}
-									type='submit'
-									className='btn btn-large waves-effect waves-light hoverable blue accent-3'
-								>
-									Sign up
-								</button>
-							</div>
+						<form className='login-form' noValidate onSubmit={this.handleOnSubmit}>
+							<input
+								onChange={this.handleOnChange}
+								value={this.state.name}
+								error={errors.name}
+								id='name'
+								type='text'
+								className={classnames('', {
+									invalid : errors.name
+								})}
+								placeholder='Username'
+							/>
+							<input
+								onChange={this.handleOnChange}
+								value={this.state.email}
+								error={errors.email}
+								id='email'
+								type='email'
+								className={classnames('', {
+									invalid : errors.email
+								})}
+								placeholder='Email'
+							/>
+							<input
+								onChange={this.handleOnChange}
+								value={this.state.password}
+								error={errors.password}
+								id='password'
+								type='password'
+								className={classnames('', {
+									invalid : errors.password
+								})}
+								placeholder='Password'
+							/>
+							<input
+								onChange={this.handleOnChange}
+								value={this.state.password2}
+								error={errors.password2}
+								id='password2'
+								type='password'
+								className={classnames('', {
+									invalid : errors.password2
+								})}
+								placeholder='Confirm Password'
+							/>
+							<button>create</button>
+							<p className='message'>Already registered? <Link to='/login'>Sign In</Link></p>
 						</form>
 					</div>
 				</div>
