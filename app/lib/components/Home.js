7190da202c8d9ba7afd46515197bbf9a56da5dd9
magnito2 <magnusotwani@gmail.com>
2020-03-09 14:41:22 +0300

    added home page

---
+++ app/lib/components/Home.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { roomsGetAll, changeName } from '../redux/roomsActions';
+import classnames from 'classnames';
+import { Link } from 'react-router-dom';
+
+import deepEqual from 'deep-equal-x';
+import { isEmpty } from '../utils';
+
+class Home extends Component
+{
+	constructor()
+	{
+		super();
+		this.state = {
+			rooms    : [],
+			roomName : ''
+		};
+
+		this.handleOnChange = this.handleOnChange.bind(this);
+		this.handleOnSubmit = this.handleOnSubmit.bind(this);
+	}
+
+	componentDidMount()
+	{
+		this.props.roomsGetAll();
+	}
+
+	static getDerivedStateFromProps(props, state)
+	{
+		if (!isEmpty(props.rooms) && !deepEqual(props.rooms, state.rooms))
+		{
+			return {
+				rooms : props.rooms
+			};
+		}
+
+		return null;
+	}
+
+	handleOnChange(e)
+	{
+		this.setState({ [e.target.id]: e.target.value });
+	}
+
+	handleOnSubmit(e)
+	{
+		e.preventDefault();
+		if (this.state.roomName)
+		{
+			this.props.changeName(this.state.roomName);
+
+			this.props.history.push(
+				{
+					pathname : '/room',
+					search   : `?roomName=${this.state.roomName}`,
+					state    : { roomName: this.state.roomName }
+				});
+		}
+	}
+
+	render()
+	{
+		const { rooms } = this.state;
+
+		return (
+			<div data-component='Home'>
+				<div className='room-logout-wrapper'>
+					<div className='room-logout'>
+						<Link
+							className='link'
+							to='/logout'
+						>
+							Logout
+						</Link>
+					</div>
+				</div>
+				<div className='container'>
+					<div className='room-list'>
+						<h3>{rooms.length ? 'Choose a room to Join' : 'There is no active room'}</h3>
+						<ul>
+							{
+								rooms.map((room, index) =>
+								{
+									return <li key={index}><a href={`/room?roomId=${room.id}`}>Room {room.name || room.id} by {room.displayName}</a></li>;
+								})
+							}
+						</ul>
+					</div>
+					<div className='new-room'>
+						<h3>Create a room</h3>
+						<form className='create-form' noValidate onSubmit={this.handleOnSubmit}>
+							<input
+								onChange={this.handleOnChange}
+								id='roomName'
+								type='text'
+								placeholder='name of class'
+							/>
+							<button>Create</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = (state) => ({
+	rooms : state.rooms
+});
+
+export default connect(
+	mapStateToProps,
+	{ roomsGetAll, changeName }
+)(Home);
