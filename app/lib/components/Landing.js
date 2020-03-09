e4999b8371ca674cea3aabfb317f50035dafd6c3
magnito2 <magnusotwani@gmail.com>
2020-03-09 15:23:42 +0300

    remove everything

--- app/lib/components/Landing.js
+++ app/lib/components/Landing.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { Container, Jumbotron, Row, Col, Card, ButtonToolbar, Button } from 'react-bootstrap';
-
 class Landing extends Component
 {
 	constructor()
@@ -11,35 +9,16 @@ class Landing extends Component
 		this.state = {};
 	}
 
+	componentDidMount()
+	{
+		this.props.history.push('/home');
+	}
+
 	render()
 	{
 		return (
 			<div data-component='Landing'>
-				<Container>
-					<Jumbotron>
-						<h1>Be Awesome, create something nice </h1>
-					</Jumbotron>
-
-					<Row className='justify-content-md-center'>
-						<Col xs={6}>
-							<Card>
-								<Card.Body>
-									<Card.Title>Card title</Card.Title>
-									<h6 className='card-subtitle mb-2 text-muted'>Card subtitle</h6>
-									<p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card&apos;s content.</p>
-									{
-										this.props.auth.isAuthenticated ?
-											<Button variant='success'>Join Room</Button> :
-											<ButtonToolbar>
-												<Button variant='primary'>Login</Button>
-												<Button variant='secondary'>Sign Up</Button>
-											</ButtonToolbar>
-									}
-								</Card.Body>
-							</Card>
-						</Col>
-					</Row>
-				</Container>
+				<h1>Hello world</h1>
 			</div>
 		);
 	}
