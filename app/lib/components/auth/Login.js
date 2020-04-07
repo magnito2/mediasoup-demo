d7adbac034ffcb97642c08c09a44cf8aa2385d8a
magnito2 <magnusotwani@gmail.com>
2020-04-07 22:20:18 +0300

    allow for admnin no and email and bug fixes, improvements

--- app/lib/components/auth/Login.js
+++ app/lib/components/auth/Login.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { loginUser, setUserLoading } from '../../redux/authActions';
+import { loginUser, setUserLoading, clearError, clearErrors } from '../../redux/authActions';
 import * as requestActions from '../../redux/requestActions';
 import classnames from 'classnames';
 
@@ -18,23 +18,29 @@ class Login extends Component
 	{
 		super();
 		this.state = {
-			email    : '',
-			password : '',
-			errors   : { },
-			isAuth   : false
+			email           : '',
+			admissionNumber : '',
+			password        : '',
+			userType        : 'student',
+			errors          : { },
+			isAuth          : false
 		};
 		this.handleOnChange = this.handleOnChange.bind(this);
 		this.handleOnSubmit = this.handleOnSubmit.bind(this);
 	}
 
-	componentDidUpdate(prevProps)
+	componentDidMount()
 	{
-
-		if (prevProps.auth.isAuthenticated)
+		if (this.props.auth.isAuthenticated)
 		{
 			this.props.onAuthenticated();
 			this.props.history.push('/home'); // push user to room when they login
 		}
+	}
+
+	componentWillUnmount()
+	{
+		this.props.onClearErrors();
 	}
 
 	static getDerivedStateFromProps(props, state)
@@ -58,14 +64,37 @@ class Login extends Component
 	handleOnChange(e)
 	{
 		this.setState({ [e.target.id]: e.target.value });
+		if (e.target.id === 'userType')
+		{
+			if (e.target.value === 'teacher')
+			{
+				this.setState({ admissionNumber: '' });
+			}
+			else if (e.target.value === 'student')
+			{
+				this.setState({ email: '' });
+			}
+		}
+		if (this.state.errors)
+		{
+			const patt = new RegExp(e.target.id);
+			const keys = Object.keys(this.state.errors).filter((error) => patt.test(error));
+
+			if (keys.length)
+			{
+				this.props.onClearError(keys[0]);
+			}
+		}
 	}
 
 	handleOnSubmit(e)
 	{
 		e.preventDefault();
 		const userData = {
-			email    : this.state.email,
-			password : this.state.password
+			userType        : this.state.userType,
+			email           : this.state.email,
+			admissionNumber : this.state.admissionNumber,
+			password        : this.state.password
 		};
 
 		this.props.onLoginUser(userData);
@@ -85,27 +114,63 @@ class Login extends Component
 							<p className='message'><Link to='/' className='icon-left'>Go Back</Link></p>
 						</div>
 						<form className='login-form' noValidate onSubmit={this.handleOnSubmit}>
-							<input
-								onChange={this.handleOnChange}
-								value={this.state.email}
-								error={errors.email}
-								id='email'
-								type='email'
-								placeholder='email'
-								className={classnames('', {
-									invalid : errors.email || errors.emailnotfound
-								})}
-							/>
 							<span className='red-text'>
-								{errors.email}
-								{errors.emailnotfound}
+								{errors.generalError}
 							</span>
+							<select
+								onChange={this.handleOnChange}
+								value={this.state.userType}
+								errors={errors.userType}
+								id='userType'
+							>
+								<option value='student'>Student</option>
+								<option value='teacher'>Teacher</option>
+							</select>
+							{this.state.userType === 'teacher' &&
+								<>
+									<input
+										onChange={this.handleOnChange}
+										value={this.state.email}
+										error={errors.email}
+										id='email'
+										type='email'
+										placeholder='email'
+										className={classnames('', {
+											invalid : errors.email || errors.emailnotfound
+										})}
+									/>
+									<span className='red-text'>
+										{errors.email}
+										{errors.emailnotfound}
+									</span>
+								</>
+							}
+							{this.state.userType === 'student' &&
+								<>
+									<input
+										onChange={this.handleOnChange}
+										value={this.state.admissionNumber}
+										error={errors.admissionNumber}
+										id='admissionNumber'
+										type='text'
+										placeholder='admission number'
+										className={classnames('', {
+											invalid : errors.admissionNumber || errors.admissionNumbernotfound
+										})}
+									/>
+									<span className='red-text'>
+										{errors.admissionNumber}
+										{errors.admissionNumbernotfound}
+									</span>
+								</>
+							}
 							<input
 								onChange={this.handleOnChange}
 								value={this.state.password}
 								error={errors.password}
 								id='password'
 								type='password'
+								placeholder='Password'
 								className={classnames('', {
 									invalid : errors.password || errors.passwordincorrect
 								})}
@@ -131,7 +196,9 @@ Login.propTypes = {
 	errors          : PropTypes.object,
 	history        	: PropTypes.object,
 	onAuthenticated : PropTypes.func,
-	onUserLoading   : PropTypes.func
+	onUserLoading   : PropTypes.func,
+	onClearError    : PropTypes.func,
+	onClearErrors   : PropTypes.func
 };
 
 const mapStateToProps = (state) => ({
@@ -156,6 +223,14 @@ const mapDispatchToProps = (dispatch) =>
 		onUserLoading : () =>
 		{
 			dispatch(setUserLoading());
+		},
+		onClearError : (errorKey) =>
+		{
+			dispatch(clearError(errorKey));
+		},
+		onClearErrors : () =>
+		{
+			dispatch(clearErrors());
 		}
 	};
 };
