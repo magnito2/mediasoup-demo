df753533cd9f1edd5d3aedd9a8ff1a35fc93e10d
magnito2 <magnusotwani@gmail.com>
2020-04-07 22:15:02 +0300

    allow for student to sign in via admin no and teacher via email

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -27,20 +27,36 @@ router.post('/register', (req, res) =>
 	{
 		return res.status(400).json(errors);
 	}
-	User.findOne({ email: req.body.email }).then((user) =>
+
+	logger.debug(`Email is ${req.body.email}`);
+
+	User.findOne({
+		$or : [
+			{ $and: [ { email: { '$exists': true, '$ne': '' } }, { email: req.body.email } ] },
+			{ $and: [ { admissionNumber: { '$ne': '' } }, { admissionNumber: req.body.admissionNumber } ] }
+		]
+	}).then((user) =>
 	{
 		if (user)
 		{
-			return res.status(400).json({ email: 'Email already exists' });
+			if (user.email && user.email === req.body.email)
+			{
+				return res.status(400).json({ email: 'Email already exists' });
+			}
+			else if (user.admissionNumber && user.admissionNumber === req.body.admissionNumber)
+			{
+				return res.status(400).json({ admissionNumber: 'Admission Number already exists' });
+			}
 		}
 		else
 		{
+			logger.debug('Creating a new user, %o', req.body);
 			const newUser = new User({
-				name         : req.body.name,
-				centreNumber : req.body.centreNumber,
-				userType     : req.body.userType,
-				email        : req.body.email,
-				password     : req.body.password
+				name            : req.body.name,
+				admissionNumber : req.body.admissionNumber,
+				userType        : req.body.userType,
+				email           : req.body.email,
+				password        : req.body.password
 			});
 			// Hash password before saving in database
 
@@ -52,8 +68,21 @@ router.post('/register', (req, res) =>
 					newUser.password = hash;
 					newUser
 						.save()
-						.then((userIn) => res.json(userIn))
-						.catch((errIn2) => logger.error(errIn2));
+						.then((userIn) => res.json({
+							name            : userIn.name,
+							admissionNumber : userIn.admissionNumber,
+							email           : userIn.email,
+							id              : userIn.id,
+							userType        : userIn.userType
+						}))
+						.catch((errIn2) =>
+						{
+							logger.error(errIn2);
+
+							return res.status(400).json({
+								generalError : errIn2.message
+							});
+						});
 				});
 			});
 		}
@@ -74,15 +103,30 @@ router.post('/login', (req, res) =>
 		return res.status(400).json(errors);
 	}
 	const email = req.body.email;
+	const admissionNumber = req.body.admissionNumber;
 	const password = req.body.password;
 	// Find user by email
 
-	User.findOne({ email }).then((user) =>
+	logger.debug(`Admin no ${admissionNumber}`);
+
+	User.findOne({ $or :
+		[
+			{ $and: [ { email }, { userType: 'teacher' } ] },
+			{ $and: [ { admissionNumber: { '$ne': '' } }, { admissionNumber }, { userType: 'student' } ] }
+		]
+	}).then((user) =>
 	{
 		// Check if user exists
 		if (!user)
 		{
-			return res.status(404).json({ emailnotfound: 'Email not found' });
+			if (email)
+			{
+				return res.status(404).json({ emailnotfound: 'Email not found' });
+			}
+			else if (admissionNumber)
+			{
+				return res.status(404).json({ admissionNumbernotfound: 'Admission Number not found' });
+			}
 		}
 		// Check password
 		bcrypt.compare(password, user.password).then((isMatch) =>
