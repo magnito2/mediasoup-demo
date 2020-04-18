40f77a9f7a7bf31352401c1931662e6ccdb563a8
magnito2 <magnusotwani@gmail.com>
2020-04-18 11:29:41 +0300

    remove port 4444

--- app/lib/urlFactory.js
+++ app/lib/urlFactory.js
@@ -1,7 +1,4 @@
-let protooPort = 4443;
-
-if (window.location.hostname === 'test.mediasoup.org')
-	protooPort = 4444;
+const protooPort = 4443;
 
 export function getProtooUrl({ roomId, peerId, forceH264, forceVP9 })
 {
